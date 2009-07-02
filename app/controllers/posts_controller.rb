028e0a131099b35463b6d7b494222837077ee5bd
Niklas Hofer <niklas+dev@lanpartei.de>
2009-07-03 01:12:01 +0200

    use nested attributes to create markes on the fly (manual position)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,6 +40,7 @@ class PostsController < ApplicationController
   # GET /posts/1/edit
   def edit
     @post = Post.find(params[:id])
+    @post.markers.build
   end
 
   # POST /posts
--- app/models/marker.rb
+++ app/models/marker.rb
@@ -3,5 +3,7 @@ class Marker < ActiveRecord::Base
   
   validates_presence_of :name
   validates_numericality_of :pos_x, :pos_y
+  validates_inclusion_of :pos_x, :in => 0..100
+  validates_inclusion_of :pos_y, :in => 0..100
   
 end
