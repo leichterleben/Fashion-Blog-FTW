43280fe9b67da9c4ad9df03225090a877249bfe3
Christian Leicht <christian+dev@lanpartei.de>
2009-07-02 23:50:43 +0200

    Show für Besucher und Admins bearbeitet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.xml
   def index
-    @posts = Post.all
+    @posts = Post.all :order => "created_at DESC"
 
     respond_to do |format|
       format.html # index.html.erb
