9c1c68fe9f9dd675b1e24b8204d357aabbfb1cc1
Niklas Hofer <niklas+dev@lanpartei.de>
2009-07-03 00:39:32 +0200

    show only published posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   has_attached_file :foto, :styles =>{:standard =>"500x500>", :thumb => "100x100>"}
   
   has_many :markers, :order => 'created_at ASC'
+
+  named_scope :published, {:conditions => {:published => true}}
 end
